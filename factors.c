389e5bbbe896c811444c3390a832750cca8e0ec5
trOyYeyY <troyzuwara@gmail.com>
2023-12-22 21:24:54 +0000

    final version

--- factors.c
+++ factors.c
@@ -1,51 +1,64 @@
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 void print_factors(long long int n)
 {
-	long long int ix;
-	if (n % 2 == 0)
-	{
-		printf("%lld=%lld*%d\n", n, n / 2, 2);
-		return;
-	}
+    if (n % 2 == 0)
+    {
+        printf("%lld=%lld*%lld\n", n, n / 2, 2LL);
+        return;
+    }
 
-	for (x = 3; x <= n / 2; x += 2)
-	{
-		if (n % i == 0)
-		{
-			printf("%lld=%lld*%lld\n", n, n / x, x);
-			break;
-		}
-	}
+    long long int i;
+    for (i = 3; i <= n / 2; i += 2)
+    {
+        if (n % i == 0)
+        {
+            printf("%lld=%lld*%lld\n", n, n / i, i);
+            return;
+        }
+    }
 }
 
 int main(int argc, char **argv)
 {
 	char *buffer = NULL;
 	FILE *fd;
-	size_t lenght = 0;
-	ssize_t status;
-	long long int num;
+	size_t l = 0;
+	ssize_t stat;
+	long long int number;
+
 	if (argc != 2)
-		return (EXIT_FAILURE);
+		return EXIT_FAILURE;
 
 	fd = fopen(argv[1], "r");
 	if (fd == NULL)
-		return (EXIT_FAILURE);
-	status = getline(&buffer, &lenght, fd);
+		return EXIT_FAILURE;
 
-	while (status != -1)
+	while ((stat = getline(&buffer, &l, fd)) != -1)
 	{
-		num = atoll(buffer);
-		free(buffer);
-		buffer = NULL;
-		lenght = 0;
-		print_factors(numb);
-		status = getline(&buffer, &lenght, fd);
+		if (stat > 0 && buffer[stat - 1] == '\n')
+			buffer[stat - 1] = '\0';
+
+		// Skip empty lines
+		if (strlen(buffer) == 0)
+		{
+		continue;
+		}
+
+		// Skip non-numeric lines
+		char *endptr;
+		number = strtoll(buffer, &endptr, 10);
+		if (endptr == buffer || *endptr != '\0')
+        		{
+			printf("Skipping non-numeric line: %s\n", buffer);
+			continue;
+			}
+        	print_factors(number);
 	}
 	fclose(fd);
-	return (EXIT_SUCCESS);
-
+	free(buffer);
+	return EXIT_SUCCESS;
 }
